7e7cb3651a4af70771173f2e9fe2a282da578a6f
Ernesto Marcelino Haughton <ernesto.marcelino@outlook.com>
2023-08-22 13:27:40 +0200

    Update list task

--- src/components/task/TaskList.vue
+++ src/components/task/TaskList.vue
@@ -2,15 +2,14 @@
 import { VueTable } from "@harv46/vue-table"
 import IconTrash from "../icons/IconTrash.vue";
 import IconEdit from "../icons/IconEdit.vue";
-import "@harv46/vue-table/dist/style.css"
-import { useRouter } from 'vue-router';
+import "@harv46/vue-table/dist/style.css";
 import axios from "axios";
 import { ref } from "vue";
 
 let getData = ref([]);
 
-const headers = ["name", "created_at"];
-const keyValue = ["name", "created_at"];
+const headers = ["Nombre","Estado", "Categoría", "Creado"];
+const keyValue = ["name","status", "category","created_at"];
 
 const filldatatable = () => axios.get("http://task-manager.test/api/task").then((result) => {
     getData.value = result.data?.data;
@@ -29,14 +28,14 @@ const deleteItem = (uuid) => {
 <template>
     <div class="grid grid-cols-1 gap-4 place-items-center h-20 w-full">
         <button @click="$router.push('/create')"
-            class="px-6 py-2 text-white bg-green-500  hover:bg-green-700 rounded-md">
+            class="px-6 py-2 text-white bg-blue-500  hover:bg-sky-700 rounded-md">
             Nueva tarea
         </button>
     </div>
 
     <VueTable :headers="headers" :keys="keyValue" :data="getData">
         <template #th>
-            <th> Actions</th>
+            <th> Acciones</th>
         </template>
         <template #td="{ item }">
             <td>
